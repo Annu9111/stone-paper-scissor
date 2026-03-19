ebc878907036e960990fb2a66f48b79957777a7f
Annu9111 <aksoni911190@gmail.com>
2026-03-19 20:02:22 +0530

    js

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ let compScore=0;
 
 const choices=document.querySelector(".choice");
 
-const playGame=() =>{
-    
+const genCompChoice=() =>{
+    const options=["rock","paper","scissor"];
+    const randindx=Math.floor(Math.random()*3);
+}
+
+const playGame=(userchoice) =>{
 }
 
 choices.forEach(choice => {
     choice.addEventListener("click",() => {
         const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
 
     })
 });

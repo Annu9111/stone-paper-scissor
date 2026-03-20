7b67b3bd1a82534b6e8cbbdb94bd91e652f46dce
Annu9111 <aksoni911190@gmail.com>
2026-03-20 09:05:25 +0530

    js

--- script.js
+++ script.js
@@ -6,9 +6,36 @@ const choices=document.querySelector(".choice");
 const genCompChoice=() =>{
     const options=["rock","paper","scissor"];
     const randindx=Math.floor(Math.random()*3);
+    return options[randindx];
 }
 
-const playGame=(userchoice) =>{
+const drawGame=() =>{
+    console.log("It's a Draw")
+}
+
+const  showWinner =(userwin)=>{
+    if (userwin){
+        console.log("You win");
+    }else{
+        console.log("You lose");
+    }
+}
+
+const playGame=(userChoice) =>{
+    const compChoice=genCompChoice();
+    if (userChoice===compChoice){
+        drawGame();
+    }else{
+        let Userwin=true;
+        if (userChoice==="rock"){
+            Userwin =compChoice ==="paper" ? false :true;
+        }else if( userChoice==="paper"){
+            Userwin=compChoice==="scissor" ? false :true;
+        }else{
+            Userwin=compChoice==="rock"? false :true;
+        }
+        showWinner(Userwin);
+    }
 }
 
 choices.forEach(choice => {
